9b41693fcd5de1a331cfa40925204c0a2f282f2a
steyoo25 <xbowmaster2020@gmail.com>
2022-04-09 16:14:18 -0400

    text Done

--- dogObjects.ts
+++ dogObjects.ts
@@ -2,43 +2,53 @@ import * as fs from 'fs';
 export class Dog {
     breedName : string;
     allergy : boolean;
-    weight : Number[];
+    weight : string;
     personality : string;
+    size : string;
+    time : string;
+    cost : string;
+    aorh : string;
+    train : string;
+    yard : string;
     point : number;
 
-    constructor(breedName : string, allergy : boolean, weight : Number[], personality : string){
+    constructor(breedName : string, allergy : boolean, weight : string, personality : string, size : string, time : string, cost : string, aorh : string, train : string, yard : string,){
         this.breedName = breedName;
         this.allergy = allergy;
         this.weight = weight;
         this.personality = personality;
+        this.size = size;
+        this.time = time;
+        this.cost = cost;
+        this.aorh = aorh;
+        this.train = train;
+        this.yard = yard;
         this.point = 0;
     }
 }
 
 export let newArray : Object[] = new Array();
-
 export let hypoAllergy : Object[] = new Array();
-
-
 
 const file = fs.readFileSync('allDogs.txt','utf-8').split('\n');
 file.forEach((line)=>{
     let l = line.split(';');
-    let dogName : string = l[0].trim();
-    let allergy : boolean = Boolean(l[1].trim());
-    let weightRange : Number[] = [Number(l[2].split(',')[0]),Number(l[2].split(',')[1])];
-    let personality : string = l[3].trim();
+    for(let i = 0; i < l.length; i++){
+        l[i] = l[i].trim();
+    }
+    let dogName : string = l[0];
+    let allergy : boolean = Boolean(l[1]);
+    let weightRange : string = l[2];
+    let personality : string = l[3];
+    let size : string = l[4];
+    let time : string = l[5];
+    let cost : string = l[6];
     let dogObject = new Dog(dogName, allergy, weightRange, personality);
     newArray.push(dogObject);
     if (allergy){
         hypoAllergy.push(dogObject)
     }
 })
-<<<<<<< HEAD
 
 console.log(hypoAllergy)
 console.log(newArray);
-
-
-=======
->>>>>>> f5580e6b3fcec89f1051ba9b66b707f0e29b9df5
