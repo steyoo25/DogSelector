565dfec8ea2ca0923d846b76e4723611c5c80259
ishaaty <ishatyagi888@gmail.com>
2022-04-09 12:48:56 -0400

    added some stuff

---
+++ userInput.ts
@@ -0,0 +1,36 @@
+const enterBtn = document.querySelector("button") as HTMLButtonElement
+
+
+document.addEventListener("click", (event: MouseEvent) =>{
+
+    let allergies = document.querySelector("#allergies") as HTMLSelectElement 
+    let a = allergies.value
+
+})
+
+
+class userInput {
+    allergy : a
+}
+
+
+ 
+
+
+
+
+
+
+/* class Dog {
+    allergy : boolean;
+    weight : Number[];
+    personality : string;
+
+    constructor(allergy : boolean, weight : Number[], personality : string){
+        this.allergy = allergy;
+        this.weight = weight;
+        this.personality = personality;
+    }
+}
+
+let dog1 = new Dog(true,[50,50],'aggressive');*/
