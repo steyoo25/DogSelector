8a41d621d141fcffc2e441517e112f9b79d206cc
steyoo25 <xbowmaster2020@gmail.com>
2022-04-09 12:44:56 -0400

    almost worked, need to finish saving it

--- algorithm.js
+++ algorithm.js
@@ -1,21 +1,31 @@
 "use strict";
 exports.__esModule = true;
 var fs = require("fs");
-fs.readFile('allDogs.txt', 'utf-8', function (err, data) {
-    if (err)
-        throw err;
-    var newArray = data.split('\n');
-    newArray.forEach(function (line) {
-        console.log(line);
-        console.log('OISDJFOSDJIFJSDIOFJDOIJDSIOFJOIF');
-    });
-});
 var Dog = /** @class */ (function () {
-    function Dog(allergy, weight, personality) {
+    function Dog(breedName, allergy, weight, personality) {
+        this.breedName = breedName;
         this.allergy = allergy;
         this.weight = weight;
         this.personality = personality;
     }
     return Dog;
 }());
-var dog1 = new Dog(true, [50, 50], 'aggressive');
+var newArray = new Array();
+var secondArray = 0;
+fs.readFile('allDogs.txt', 'utf-8', function (err, data) {
+    if (err)
+        throw err;
+    var dogs = data.split('\n');
+    dogs.forEach(function (line) {
+        var l = line.split(';');
+        var dogName = l[0].trim();
+        var allergy = Boolean(l[1].trim());
+        var weightRange = [Number(l[2].split(',')[0]), Number(l[2].split(',')[1])];
+        var personality = l[3].trim();
+        var dogObject = new Dog(dogName, allergy, weightRange, personality);
+        newArray.push(dogObject);
+    });
+    console.log(newArray);
+    secondArray = newArray;
+});
+console.log(secondArray);
--- algorithm.ts
+++ algorithm.ts
@@ -1,24 +1,30 @@
 import * as fs from 'fs';
-
-fs.readFile('allDogs.txt','utf-8',(err,data)=>{
-    if (err) throw err;
-    let dogs : string[] = data.split('\n');
-    dogs.forEach((line)=>{
-        console.log(line);
-        console.log('OISDJFOSDJIFJSDIOFJDOIJDSIOFJOIF');
-    });
-});
-
 class Dog {
+    breedName : string;
     allergy : boolean;
     weight : Number[];
     personality : string;
 
-    constructor(allergy : boolean, weight : Number[], personality : string){
+    constructor(breedName : string, allergy : boolean, weight : Number[], personality : string){
+        this.breedName = breedName;
         this.allergy = allergy;
         this.weight = weight;
         this.personality = personality;
     }
 }
-
-let dog1 = new Dog(true,[50,50],'aggressive');
+let newArray : Object[] = new Array();
+let secondArray : Object = 0;
+fs.readFile('allDogs.txt','utf-8',(err,data)=>{
+    if (err) throw err;
+    let dogs : string[] = data.split('\n');
+    dogs.forEach((line)=>{
+        let l = line.split(';');
+        let dogName : string = l[0].trim();
+        let allergy : boolean = Boolean(l[1].trim());
+        let weightRange : number[] = [Number(l[2].split(',')[0]),Number(l[2].split(',')[1])];
+        let personality : string = l[3].trim();
+        let dogObject = new Dog(dogName, allergy, weightRange, personality);
+        newArray.push(dogObject);
+    });
+    console.log(newArray);
+});
